9f3a69042444055211f8d7399f65dfce4fd4002e
Junaid Farooq <junaid@evercam.io>
2016-10-20 18:08:00 +0500

    Added changeset & update_extractor_status method

--- web/models/snapshot_extractor.ex
+++ web/models/snapshot_extractor.ex
@@ -4,6 +4,9 @@ defmodule SnapshotExtractor do
   import Ecto.Query
   alias Extractor.Repo
 
+  @required_fields ~w(status)
+  @optional_fields ~w(notes)
+
   schema "snapshot_extractors" do
     field :camera_id, :integer
     field :from_date, Ecto.DateTime, default: Ecto.DateTime.utc
@@ -27,5 +30,19 @@ defmodule SnapshotExtractor do
     |> join(:inner_lateral, [se], cam in fragment("SELECT * FROM cameras as cam WHERE cam.id = ?", se.camera_id))
     |> select([se, cam], %{ from_date: se.from_date, to_date: se.to_date, interval: se.interval, schedule: se.schedule, camera_exid: cam.exid, timezone: cam.timezone})
     |> Repo.one
+  end
+
+  def update_extractor_status(params) do
+    SnapshotExtractor
+    |> order_by(desc: :created_at)
+    |> limit(1)
+    |> Repo.one
+    |> changeset(params)
+    |> Repo.update
+  end
+
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
   end
 end
