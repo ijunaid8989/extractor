edade4badc100542dbbadbb18beb4c98c59298e9
Junaid Farooq <junaid@evercam.io>
2016-10-25 23:53:50 +0500

    Removed Extractor Details and Added Robert Frost

--- web/controllers/snapshot_extractor_controller.ex
+++ web/controllers/snapshot_extractor_controller.ex
@@ -2,7 +2,6 @@ defmodule Extractor.SnapshotExtractorController do
   use Extractor.Web, :controller
 
   def index(conn, _params) do
-    snapshot_extractors = SnapshotExtractor.all_extractors
-    render conn, "extractor.html", snapshot_extractors: snapshot_extractors
+    render conn, "extractor.html"
   end
 end
