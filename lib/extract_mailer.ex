6e98d83207b8017d3145f737471f47b64a9b9c53
Junaid Farooq <junaid@evercam.io>
2016-11-23 14:27:06 +0500

    Added expected count reference to mailer and email templates.

--- lib/extract_mailer.ex
+++ lib/extract_mailer.ex
@@ -14,13 +14,13 @@ defmodule Extractor.ExtractMailer do
       text: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_started.txt", year: @year)
   end
 
-  def extractor_completed(count, camera_name) do
+  def extractor_completed(count, camera_name, expected_count) do
     Mailgun.Client.send_email @config,
       to: "junaid@evercam.io",
       subject: "SnapShot Extraction Completed",
       from: @from,
       bcc: "marco@evercam.io,info@lensmen.ie",
-      html: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.html", count: count, camera_name: camera_name),
-      text: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.txt", count: count, camera_name: camera_name)
+      html: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.html", count: count, camera_name: camera_name, expected_count: expected_count),
+      text: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.txt", count: count, camera_name: camera_name, expected_count: expected_count)
   end
 end
