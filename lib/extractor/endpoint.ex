8ee2d68647af84ff01586c56293a4cf7aef43b19
Junaid Farooq <junaid@evercam.io>
2016-12-08 14:06:45 +0500

    Get image back to root

--- lib/extractor/endpoint.ex
+++ lib/extractor/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Extractor.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :extractor, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt image.jpg)
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
