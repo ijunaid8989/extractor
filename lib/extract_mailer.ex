9948f755e7c925499d7c5c3fea5898cfa3ad3634
Junaid Farooq <junaid@evercam.io>
2017-01-17 23:34:23 +0500

    Added DropBox Url in Completion email.

--- lib/extract_mailer.ex
+++ lib/extract_mailer.ex
@@ -14,13 +14,13 @@ defmodule Extractor.ExtractMailer do
       text: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_started.txt", start_date: e_start_date, to_date: e_to_date, interval: e_interval, schedule: e_schedule, camera_name: camera_name, requestor: requestor)
   end
 
-  def extractor_completed(count, camera_name, expected_count) do
+  def extractor_completed(count, camera_name, expected_count, extractor_id, camera_exid) do
     Mailgun.Client.send_email @config,
       to: "junaid@evercam.io",
       subject: "SnapShot Extraction Completed",
       from: @from,
       bcc: "marco@evercam.io,info@lensmen.ie",
-      html: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.html", count: count, camera_name: camera_name, expected_count: expected_count),
-      text: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.txt", count: count, camera_name: camera_name, expected_count: expected_count)
+      html: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.html", count: count, camera_name: camera_name, expected_count: expected_count, extractor_id: extractor_id, camera_exid: camera_exid),
+      text: Phoenix.View.render_to_string(Extractor.EmailView, "extractor_completed.txt", count: count, camera_name: camera_name, expected_count: expected_count, extractor_id: extractor_id, camera_exid: camera_exid)
   end
 end
