e625364fde91ad661d0558e45e40526fc2246fd7
Junaid Farooq <junaid@evercam.io>
2017-04-02 07:16:55 +0500

    Added exrm to build the release.

--- mix.exs
+++ mix.exs
@@ -53,6 +53,7 @@ defmodule Extractor.Mixfile do
       {:phoenix, "~> 1.2.1"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.0"},
+      {:exrm, github: "bitwalker/exrm"},
       {:postgrex, ">= 0.11.2"},
       {:phoenix_html, "~> 2.6"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
