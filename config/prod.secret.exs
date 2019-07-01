344aad7e53c8f8ed6bb9f3823ffaa4060fd4efc1
Junaid Farooq <junaid@evercam.io>
2019-07-01 17:55:28 +0500

    Reduced PoolSize to 5.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -19,6 +19,6 @@ config :extractor, Extractor.Repo,
   socket_options: [keepalive: true],
   timeout: 60_000,
   pool_timeout: 60_000,
-  pool_size: 20,
+  pool_size: 5,
   lazy: false,
   ssl: true
